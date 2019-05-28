550ff3f6e8f63e4e2214cc796ddb840832f19699
Shalser <shalser82@yandex.ru>
2019-05-28 13:31:28 +0300

    add files

---
+++ CreativeAgency/js/off_hvr-grow.js
@@ -0,0 +1,84 @@
+// $(document).ready(function() {
+ 
+//   $(window).resize(function(){
+//     var windowWidth = $(window).width();
+//     if(windowWidth > 992)$(".header-left_block-middle_text-right").addClass("hvr-grow");
+//     else $(".header-left_block-middle_text-right").removeClass("hvr-grow");
+//   });
+// });
+
+// $(window).resize(function () {
+//     if($(window).width() <= 768){
+//         $('.header-left_block-middle_text-right').removeClass('hvr-grow');
+//     }
+// });
+
+// $(window).resize(function () {
+//     if($(window).width() <= 768){
+//         $('.header-left_block-subtitle_text').removeClass('hvr-grow');
+//     }
+// });
+
+$(document).ready(function() {
+ 
+    $(window).resize(function(){
+      var windowWidth = $(window).width();
+      if(windowWidth > 992)$(".header-left_block-subtitle_text").addClass("hvr-grow");
+      else $(".header-left_block-subtitle_text").removeClass("hvr-grow");
+    });
+  });
+
+$(document).ready(function() {
+ 
+    $(window).resize(function(){
+      var windowWidth = $(window).width();
+      if(windowWidth > 992)$(".header-left_block-middle_text-right").addClass("hvr-grow");
+      else $(".header-left_block-middle_text-right").removeClass("hvr-grow");
+    });
+  }); 
+  
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      var windowWidth = $(window).width();
+      if(windowWidth > 992)$(".cancel_a").addClass("hvr-grow");
+      else $(".cancel_a").removeClass("hvr-grow");
+    });
+  });   
+
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      var windowWidth = $(window).width();
+      if(windowWidth > 992)$(".services-carts_cart").addClass("wow bounceInRight");
+      else $(".services-carts_cart").removeClass("wow bounceInRight");
+    });
+  });  
+
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      var windowWidth = $(window).width();
+      if(windowWidth > 992)$(".services-carts_cart").addClass("wow bounceInDown");
+      else $(".services-carts_cart").removeClass("wow bounceInDown");
+    });
+  });
+
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      var windowWidth = $(window).width();
+      if(windowWidth > 992)$(".services-carts_cart").addClass("wow bounceInLeft");
+      else $(".services-carts_cart").removeClass("wow bounceInLeft");
+    });
+  });  
+
+
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      var windowWidth = $(window).width();
+      if(windowWidth > 992)$(".services-carts-down").addClass("wow bounceInUp");
+      else $(".services-carts-down").removeClass("wow bounceInUp");
+    });
+  });   
