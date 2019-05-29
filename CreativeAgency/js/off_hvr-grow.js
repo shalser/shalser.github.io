8444dd9e9807603a7d0e757b0704e3da82915730
Shalser <shalser82@yandex.ru>
2019-05-29 11:37:10 +0300

    fix

--- CreativeAgency/js/off_hvr-grow.js
+++ CreativeAgency/js/off_hvr-grow.js
@@ -1,7 +1,7 @@
 // $(document).ready(function() {
  
 //   $(window).resize(function(){
-//     var windowWidth = $(window).width();
+//     let windowWidth = $(window).width();
 //     if(windowWidth > 992)$(".header-left_block-middle_text-right").addClass("hvr-grow");
 //     else $(".header-left_block-middle_text-right").removeClass("hvr-grow");
 //   });
@@ -22,7 +22,7 @@
 $(document).ready(function() {
  
     $(window).resize(function(){
-      var windowWidth = $(window).width();
+      let windowWidth = $(window).width();
       if(windowWidth > 992)$(".header-left_block-subtitle_text").addClass("hvr-grow");
       else $(".header-left_block-subtitle_text").removeClass("hvr-grow");
     });
@@ -31,7 +31,7 @@ $(document).ready(function() {
 $(document).ready(function() {
  
     $(window).resize(function(){
-      var windowWidth = $(window).width();
+      let windowWidth = $(window).width();
       if(windowWidth > 992)$(".header-left_block-middle_text-right").addClass("hvr-grow");
       else $(".header-left_block-middle_text-right").removeClass("hvr-grow");
     });
@@ -40,45 +40,56 @@ $(document).ready(function() {
   $(document).ready(function() {
  
     $(window).resize(function(){
-      var windowWidth = $(window).width();
+      let windowWidth = $(window).width();
       if(windowWidth > 992)$(".cancel_a").addClass("hvr-grow");
       else $(".cancel_a").removeClass("hvr-grow");
     });
   });   
 
-  // $(document).ready(function() {
-  //
-  //   $(window).resize(function(){
-  //     var windowWidth = $(window).width();
-  //     if(windowWidth > 992)$(".services-carts_cart").addClass("wow bounceInRight");
-  //     else $(".services-carts_cart").removeClass("wow bounceInRight");
-  //   });
-  // });
-  //
-  // $(document).ready(function() {
-  //
-  //   $(window).resize(function(){
-  //     var windowWidth = $(window).width();
-  //     if(windowWidth > 992)$(".services-carts_cart").addClass("wow bounceInDown");
-  //     else $(".services-carts_cart").removeClass("wow bounceInDown");
-  //   });
-  // });
-  //
-  // $(document).ready(function() {
-  //
-  //   $(window).resize(function(){
-  //     var windowWidth = $(window).width();
-  //     if(windowWidth > 992)$(".services-carts_cart").addClass("wow bounceInLeft");
-  //     else $(".services-carts_cart").removeClass("wow bounceInLeft");
-  //   });
-  // });
-  //
-  //
-  // $(document).ready(function() {
-  //
-  //   $(window).resize(function(){
-  //     var windowWidth = $(window).width();
-  //     if(windowWidth > 992)$(".services-carts-down").addClass("wow bounceInUp");
-  //     else $(".services-carts-down").removeClass("wow bounceInUp");
-  //   });
-  // });
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      let windowWidth = $(window).width();
+      if(windowWidth > 992)$(".services-carts_cart").addClass
+      ("wow bounceInRight");
+      else $(".services-carts_cart").removeClass("wow bounceInRight");
+    });
+  });  
+
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      let windowWidth = $(window).width();
+      if(windowWidth > 992)$(".services-carts_cart").addClass("wow bounceInDown");
+      else $(".services-carts_cart").removeClass("wow bounceInDown");
+    });
+  });
+
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      let windowWidth = $(window).width();
+      if(windowWidth > 992)$(".services-carts_cart").addClass("wow bounceInLeft");
+      else $(".services-carts_cart").removeClass("wow bounceInLeft");
+    });
+  });  
+
+
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      let windowWidth = $(window).width();
+      if(windowWidth > 992)$(".services-carts-down").addClass("wow bounceInUp");
+      else $(".services-carts-down").removeClass("wow bounceInUp");
+    });
+  });  
+  
+  $(document).ready(function() {
+ 
+    $(window).resize(function(){
+      let windowWidth = $(window).width();
+      if(windowWidth > 992)$(".team-foto_foto").addClass
+      ("wow rollIn");
+      else $(".team-foto_foto").removeClass("wow rollIn");
+    });
+  });  
