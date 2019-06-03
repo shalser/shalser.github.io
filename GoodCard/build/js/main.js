65bd154fb53c798e84385556f1253ad4d90c444d
Shalser <shalser82@yandex.ru>
2019-06-03 21:10:48 +0300

    fix

--- GoodCard/build/js/main.js
+++ GoodCard/build/js/main.js
@@ -20,27 +20,39 @@ popupClose.addEventListener('click', popupHide);
 
 // menu w320
 
-// let menu = document.getElementById('menu-w320');
-// let menuButton = document.getElementById('nav-w320');
+let width = document.documentElement.clientWidth;
+let addIdW320 = document.getElementById('menu-w320');
+let overlayW320 = document.getElementById('overlay-width320');
 
-// function menuShow() {
-//     if (menu.style.display == "none") {
-//         menu.style.display = "block";
-//     } else {
-//         menu.style.display = "none";
-//     }
+if (screen.width <= 320 || width <= 320) {
+    addIdW320.id = "w320";
+    w320.style.display = "none";
+    // overlayW320.id = "overlayW320";
+} 
+
+let menu320 = document.getElementById('w320');
+let menuButton320 = document.getElementById('nav-w320');
+
+function menuShow320() {
+    if (menu320.style.display == "none" && overlayW320.style.display == "none") {
+        menu320.style.display = "block";
+        overlayW320.style.display = "block";
+    } else {
+        menu320.style.display = "none";
+        overlayW320.style.display = "none";
+    }
     
-// }
+}
 
-// menuButton.addEventListener('click', menuShow);
+menuButton320.addEventListener('click', menuShow320);
 
+// end menu-w320
 
 // overlay-w320
-// let width = document.documentElement.clientWidth;
 
-// if (screen.width <= 320 || width <= 320) {
-//     popupShow320();
-// } 
+
+
+// end overlay-w320
 
 // let popup320 = document.getElementById('overlay-w320');
 // let popupw320 = document.getElementById('popup-w320');
@@ -56,6 +68,11 @@ popupClose.addEventListener('click', popupHide);
 //     popup320.style.display = "none";
 //     popupw320.style.display = "none";
 // }
+
+// function popupShow320() {
+//         popup320.style.display = "block";
+//         popupw320.style.display = "block";
+//     }
 
 
 // w320.addEventListener('click', popupShow320);
@@ -91,6 +108,12 @@ popupClose.addEventListener('click', popupHide);
 // popup768Close.addEventListener('click', popupHide768);
 
 // menu w768
+
+// let width = document.documentElement.clientWidth;
+
+// if (screen.width <= 768 || width <= 768) {
+//     popupShow320();
+// } 
 
 let menu = document.getElementById('menu-w768');
 let menuButton = document.getElementById('nav-w768');
