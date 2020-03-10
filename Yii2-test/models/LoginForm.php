ae28e8a5842376f0db4bc0d7d9ae2a2b315bb182
Alexandr <shalser82@gmail.com>
2020-03-10 15:03:49 +0300

    add project

---
+++ Yii2-test/models/LoginForm.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * LoginForm is the model behind the login form.
+ *
+ * @property User|null $user This property is read-only.
+ *
+ */
+class LoginForm extends Model
+{
+    public $username;
+    public $password;
+    public $rememberMe = true;
+    public $reCaptcha;
+
+    private $_user = false;
+
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            // username and password are both required
+            [['username', 'password'], 'required'],
+            // rememberMe must be a boolean value
+            ['rememberMe', 'boolean'],
+            // password is validated by validatePassword()
+            ['password', 'validatePassword'],
+//            // verifyCode needs to be entered correctly
+//            ['verifyCode', 'captcha'],
+            [['reCaptcha'], \himiklab\yii2\recaptcha\ReCaptchaValidator2::className(),
+                'secret' => '6LcVK-AUAAAAACSPp_BvCnw24XdPUt2l7si3hPf8', // unnecessary if reСaptcha is already configured
+                'uncheckedMessage' => 'Please confirm that you are not a bot.'],
+        ];
+    }
+
+//    /**
+//     * @return array customized attribute labels
+//     */
+//    public function attributeLabels()
+//    {
+//        return [
+//            'verifyCode' => 'Verification Code',
+//        ];
+//    }
+
+
+    /**
+     * Validates the password.
+     * This method serves as the inline validation for password.
+     *
+     * @param string $attribute the attribute currently being validated
+     * @param array $params the additional name-value pairs given in the rule
+     */
+    public function validatePassword($attribute, $params)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->getUser();
+
+            if (!$user || !$user->validatePassword($this->password)) {
+                $this->addError($attribute, 'Incorrect username or password.');
+            }
+        }
+    }
+
+    /**
+     * Logs in a user using the provided username and password.
+     * @return bool whether the user is logged in successfully
+     */
+    public function login()
+    {
+        if ($this->validate()) {
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+        }
+        return false;
+    }
+
+    /**
+     * Finds user by [[username]]
+     *
+     * @return User|null
+     */
+    public function getUser()
+    {
+        if ($this->_user === false) {
+            $this->_user = User::findByUsername($this->username);
+        }
+
+        return $this->_user;
+    }
+}
---
+++ Yii2-test/views/site/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'App';
+?>
+
+<div class="container"><h1>App</h1></div>
+
+
+
