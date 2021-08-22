f2bdd558966d04ed8e848df502ce33abea3aa614
Александр <shalser82@gmail.com>
2021-08-23 02:15:27 +0300

    add semicolon

--- XO/script.js
+++ XO/script.js
@@ -1,4 +1,4 @@
-const app = document.querySelector('.grid')
+const app = document.querySelector('.grid');
 
 //field blocks
 const a = document.querySelector('.a');
@@ -17,9 +17,9 @@ const o = document.querySelector('.o-button');
 
 //block title
 const div = document.createElement('div');
-div.className = 'titleApp'
-div.innerHTML = 'X0'
-document.body.prepend(div)
+div.className = 'titleApp';
+div.innerHTML = 'X0';
+document.body.prepend(div);
 
 //flag win
 let win = false;
@@ -47,7 +47,7 @@ function popUp(title) {
     popupButton.innerText = 'Click';
     popupButton.addEventListener('click', () => {
         location.reload();
-    })
+    });
 }
 
 
@@ -56,7 +56,7 @@ let step = 0;
 
 
 function go(player) {
-    console.log(`${'Первым ходит ' + player}`)
+    console.log(`${'Первым ходит ' + player}`);
     app.addEventListener('click', (event) => {
         let position = event.target;
         if (position.classList.contains('x') || position.classList.contains('o')) {
@@ -83,140 +83,140 @@ function go(player) {
         } else {
             player = 'x';
         }
-    })
+    });
 }
 
 
 function winX() {
     if (a.classList.contains('x') && b.classList.contains('x') && c.classList.contains('x')) {
-        a.classList.add('winX')
-        b.classList.add('winX')
-        c.classList.add('winX')
+        a.classList.add('winX');
+        b.classList.add('winX');
+        c.classList.add('winX');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Крестики выиграли');
     }
     if (d.classList.contains('x') && e.classList.contains('x') && f.classList.contains('x')) {
-        d.classList.add('winX')
-        e.classList.add('winX')
-        f.classList.add('winX')
+        d.classList.add('winX');
+        e.classList.add('winX');
+        f.classList.add('winX');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Крестики выиграли');
     }
     if (g.classList.contains('x') && h.classList.contains('x') && i.classList.contains('x')) {
-        g.classList.add('winX')
-        h.classList.add('winX')
-        i.classList.add('winX')
+        g.classList.add('winX');
+        h.classList.add('winX');
+        i.classList.add('winX');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Крестики выиграли');
     }
     if (a.classList.contains('x') && d.classList.contains('x') && g.classList.contains('x')) {
-        a.classList.add('winX')
-        d.classList.add('winX')
-        g.classList.add('winX')
+        a.classList.add('winX');
+        d.classList.add('winX');
+        g.classList.add('winX');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Крестики выиграли');
     }
     if (b.classList.contains('x') && e.classList.contains('x') && h.classList.contains('x')) {
-        b.classList.add('winX')
-        e.classList.add('winX')
-        h.classList.add('winX')
+        b.classList.add('winX');
+        e.classList.add('winX');
+        h.classList.add('winX');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Крестики выиграли');
     }
     if (c.classList.contains('x') && f.classList.contains('x') && i.classList.contains('x')) {
-        c.classList.add('winX')
-        f.classList.add('winX')
-        i.classList.add('winX')
+        c.classList.add('winX');
+        f.classList.add('winX');
+        i.classList.add('winX');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Крестики выиграли');
     }
     if (a.classList.contains('x') && e.classList.contains('x') && i.classList.contains('x')) {
-        a.classList.add('winX')
-        e.classList.add('winX')
-        i.classList.add('winX')
+        a.classList.add('winX');
+        e.classList.add('winX');
+        i.classList.add('winX');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Крестики выиграли');
     }
     if (g.classList.contains('x') && e.classList.contains('x') && c.classList.contains('x')) {
-        g.classList.add('winX')
-        e.classList.add('winX')
-        c.classList.add('winX')
+        g.classList.add('winX');
+        e.classList.add('winX');
+        c.classList.add('winX');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Крестики выиграли');
     }
 }
 
 function winO() {
     if (a.classList.contains('o') && b.classList.contains('o') && c.classList.contains('o')) {
-        a.classList.add('winO')
-        b.classList.add('winO')
-        c.classList.add('winO')
+        a.classList.add('winO');
+        b.classList.add('winO');
+        c.classList.add('winO');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Нолики выиграли');
     }
     if (d.classList.contains('o') && e.classList.contains('o') && f.classList.contains('o')) {
-        d.classList.add('winO')
-        e.classList.add('winO')
-        f.classList.add('winO')
+        d.classList.add('winO');
+        e.classList.add('winO');
+        f.classList.add('winO');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Нолики выиграли');
     }
     if (g.classList.contains('o') && h.classList.contains('o') && i.classList.contains('o')) {
-        g.classList.add('winO')
-        h.classList.add('winO')
-        i.classList.add('winO')
+        g.classList.add('winO');
+        h.classList.add('winO');
+        i.classList.add('winO');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Нолики выиграли');
     }
     if (a.classList.contains('o') && d.classList.contains('o') && g.classList.contains('o')) {
-        a.classList.add('winO')
-        d.classList.add('winO')
-        g.classList.add('winO')
+        a.classList.add('winO');
+        d.classList.add('winO');
+        g.classList.add('winO');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Нолики выиграли');
     }
     if (b.classList.contains('o') && e.classList.contains('o') && h.classList.contains('o')) {
-        b.classList.add('winO')
-        e.classList.add('winO')
-        h.classList.add('winO')
+        b.classList.add('winO');
+        e.classList.add('winO');
+        h.classList.add('winO');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Нолики выиграли');
     }
     if (c.classList.contains('o') && f.classList.contains('o') && i.classList.contains('o')) {
-        c.classList.add('winO')
-        f.classList.add('winO')
-        i.classList.add('winO')
+        c.classList.add('winO');
+        f.classList.add('winO');
+        i.classList.add('winO');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Нолики выиграли');
     }
     if (a.classList.contains('o') && e.classList.contains('o') && i.classList.contains('o')) {
-        a.classList.add('winO')
-        e.classList.add('winO')
-        i.classList.add('winO')
+        a.classList.add('winO');
+        e.classList.add('winO');
+        i.classList.add('winO');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Нолики выиграли');
     }
     if (g.classList.contains('o') && e.classList.contains('o') && c.classList.contains('o')) {
-        g.classList.add('winO')
-        e.classList.add('winO')
-        c.classList.add('winO')
+        g.classList.add('winO');
+        e.classList.add('winO');
+        c.classList.add('winO');
         win = true;
-        app.classList.add('pe')
+        app.classList.add('pe');
         popUp('Нолики выиграли');
     }
 }
